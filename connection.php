96ad6a17a83abc0b452bb57241268854b963690d
gehan35 <115164221+gehan35@users.noreply.github.com>
2022-10-08 12:04:09 +0800

    New files added

--- connection.php
+++ connection.php
@@ -1,14 +1,34 @@
 <?php
-	try{
-		$dsn = "mysql dbname =id19677888_zandidapscoutsinfo	 ; host = localhost";
-		$user:"id19677888_zandidap	";
-		$pswd:"A8c(uiKeQtzR{}]L";
+	$idno = filter_input(INPUT_POST,'idno');
+	$firstname = filter_input(INPUT_POST,'firstname');
+	$middlename = filter_input(INPUT_POST,'middlename');
+	$lastname = filter_input(INPUT_POST,'lastname');
+	$age = filter_input(INPUT_POST,'age');
+	$birthday = filter_input(INPUT_POST,'birthday');
+	$address = filter_input(INPUT_POST,'address');
+	$cellno = filter_input(INPUT_POST,'cellno');
+	$outfitno = filter_input(INPUT_POST,'outfitno');
+	$currentrank = filter_input(INPUT_POST,'currentrank');
+	$outfitadvisor = filter_input(INPUT_POST,'outfitadvisor');
+	
+	$host = "localhost";
+	$dbusername="id19677888_zandidap";
+	$dbpassword="A8c(uiKeQtzR{}]L";
+	$dbname="id19677888_zandidapscoutsinfo";
 
-		$conn = new PD0($dsn, $user, $pswd);
-
-		$conn -> quert("USE id19677888_zandidapscoutsinfo")
+	$conn = new mysqli ($host,$dbusername,$dbpassword,$dbname);
+	if(mysqli_connect_error()){
+		die('Connection Failed' ('.mysqli_connect_error'.')'
+			.mysqli_connect_error());
 	}
-	catch(PD0Exception $e){
-		die("Error Connecting: ".$e->getMessage());
+	else{
+		$sql = "INSERT INTO zandidapScouts(idno, firstname,middlename,lastname,age,birthday,address,cellno,outfitno,currentrank,outfitadvisor)
+		values = ('$idno','$firstname','$middlename','$lastname',,'$age','$birthday','$address','$cellno','$outfitno','$currentrank','$outfitadvisor')";
+	if($conn->query($sql)){
+		echo "New Scout is added successfully!";
 	}
+	else{
+		echo"Information Error!".$sql."<br>".$conn->error;
+	}
+	$conn->close();
 ?>
