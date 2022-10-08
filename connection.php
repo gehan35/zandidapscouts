a4269f817047fa5555d6092c3fd1738c29ae3f1c
gehan35 <115164221+gehan35@users.noreply.github.com>
2022-10-08 12:20:56 +0800

    New line

--- connection.php
+++ connection.php
@@ -5,6 +5,7 @@
 	$lastname = filter_input(INPUT_POST,'lastname');
 	$age = filter_input(INPUT_POST,'age');
 	$birthday = filter_input(INPUT_POST,'birthday');
+	$school = filter_input(INPUT_POST,'school');
 	$address = filter_input(INPUT_POST,'address');
 	$cellno = filter_input(INPUT_POST,'cellno');
 	$outfitno = filter_input(INPUT_POST,'outfitno');
@@ -22,8 +23,8 @@
 			.mysqli_connect_error());
 	}
 	else{
-		$sql = "INSERT INTO zandidapScouts(idno, firstname,middlename,lastname,age,birthday,address,cellno,outfitno,currentrank,outfitadvisor)
-		values = ('$idno','$firstname','$middlename','$lastname',,'$age','$birthday','$address','$cellno','$outfitno','$currentrank','$outfitadvisor')";
+		$sql = "INSERT INTO zandidapScouts(idno, firstname,middlename,lastname,age,birthday,school,address,cellno,outfitno,currentrank,outfitadvisor)
+		values = ('$idno','$firstname','$middlename','$lastname',,'$age','$birthday', 'school','$address','$cellno','$outfitno','$currentrank','$outfitadvisor')";
 	if($conn->query($sql)){
 		echo "New Scout is added successfully!";
 	}
